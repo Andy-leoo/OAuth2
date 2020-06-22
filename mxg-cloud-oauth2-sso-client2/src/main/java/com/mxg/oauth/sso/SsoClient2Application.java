e86aa7620038ba9c1b5181adf28ff8bd5419906e
Andy-leoo <865756596@qq.com>
2020-06-22 19:50:16 +0800

    分布式认证授权， 重构 client2 客户端

--- mxg-cloud-oauth2-sso-client2/src/main/java/com/mxg/oauth/sso/SsoClient2Application.java
+++ mxg-cloud-oauth2-sso-client2/src/main/java/com/mxg/oauth/sso/SsoClient2Application.java
@@ -2,9 +2,11 @@ package com.mxg.oauth.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class SsoClient2Application {
 
